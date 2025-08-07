c0b4a359fb1f7d63f5c29fc6b02f31f4c2f33de7
Ahmadjonov Sobirjon <ahmadjonovsobirjon24@gmailcom>
2025-08-07 15:59:53 +0500

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { OtpService } from './modules/auth/otp.service';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '';
   }
 }
